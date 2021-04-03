82ce161da9ca2b8e6262eed71b7f4c3d09ddd3e3
Rivaildo Sousa <rivaildojunior@yahoo.com.br>
2021-04-03 20:40:51 -0300

    Terceiro commit

--- src/main/java/com/example/crudjenkins/controller/UsuarioController.java
+++ src/main/java/com/example/crudjenkins/controller/UsuarioController.java
@@ -1,17 +1,48 @@
 package com.example.crudjenkins.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.crudjenkins.model.Usuario;
+import com.example.crudjenkins.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
-	
-	
-	@GetMapping
+
+	private UsuarioRepository usuarioRepository;
+
+	@GetMapping("/ola")
 	public String olaMundo() {
 		return "Olá Mundo";
+	}
+
+	public UsuarioController(UsuarioRepository usuarioRepository) {
+		this.usuarioRepository = usuarioRepository;
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Usuario save(@RequestBody Usuario input) {
+		return this.usuarioRepository.save(input);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> findById(@PathVariable Long id) {
+		return ResponseEntity.ok().body(this.usuarioRepository.findById(id).get());
+	}
+
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Iterable<Usuario>> findAll() {
+		return ResponseEntity.ok().body(this.usuarioRepository.findAll());
 	}
 
 }
---
+++ src/main/java/com/example/crudjenkins/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.example.crudjenkins.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.crudjenkins.model.Usuario;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
+
+}
