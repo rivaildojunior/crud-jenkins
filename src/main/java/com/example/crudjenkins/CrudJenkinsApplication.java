c85b5fae8273effe3f018063253d62f47726ac6e
rivaildojunior <59898958+rivaildojunior@users.noreply.github.com>
2021-04-08 21:46:50 -0300

    Update CrudJenkinsApplication.java

--- src/main/java/com/example/crudjenkins/CrudJenkinsApplication.java
+++ src/main/java/com/example/crudjenkins/CrudJenkinsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class CrudJenkinsApplication extends SpringBootServletInitializer {
-
+//teste
 	public static void main(String[] args) {
 		SpringApplication.run(CrudJenkinsApplication.class, args);
 	}
